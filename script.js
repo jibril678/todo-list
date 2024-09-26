f1e0dd2695dc86e81d51f04d2e1e806aeac54f16
Jibril <jibrilabdi01@gmail.com>
2024-09-26 16:22:13 +0100

    update code readability

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
 const button = document.getElementById("create-button");
+button.addEventListener("click", () => createNewTodo());
 
 function createNewTodo() {
   const formText = document.getElementById("new-todo");
-  const newTodo = formText.value
+  const newTodo = formText.value;
   const text = document.createTextNode(newTodo);
-
   const list = document.getElementById("list");
   const listItem = document.createElement("li");
 
   listItem.appendChild(text);
   list.appendChild(listItem);
-  formText.value = ""
+  formText.value = "";
 }
-
-button.addEventListener("click", () => createNewTodo());
