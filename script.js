265cbeae2434944cf311af1190808e6de83433a2
Jibril <jibrilabdi01@gmail.com>
2024-10-01 19:00:34 +0100

    update styling and add checkbox & del button

--- script.js
+++ script.js
@@ -2,24 +2,55 @@
 const button = document.getElementById("add-task");
 const formText = document.getElementById("new-todo");
 
+// Create new task
 function createNewTodo() {
   const newTodo = formText.value;
-  const text = document.createTextNode(newTodo);
-  const listContainer = document.getElementById("list-container");
   const list = document.getElementById("list");
-  const listItem = document.createElement("li");
 
-  if (text.length == 0) {
+  if (newTodo.length == 0) {
     alert("Please Enter a Task");
-  } else {
-    listItem.appendChild(text);
-    list.appendChild(listItem);
-    listContainer.appendChild(list);
-    formText.value = "";
+    return;
   }
+
+  // Create list items and checkbox
+  const listItem = document.createElement("li");
+  listItem.classList.add("list-item");
+
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+
+  const textSpan = document.createElement("span");
+  textSpan.textContent = newTodo
+
+  // Create a delete task button
+  const deleteButton = document.createElement("button");
+  deleteButton.classList.add("delete-button");
+  deleteButton.textContent = "X";
+
+  checkbox.addEventListener("change", () => {
+    if (checkbox.checked) {
+        textSpan.classList.add("task-complete"); // Add strikethrough to text span
+    } else {
+        textSpan.classList.remove("task-complete"); // Remove strikethrough from text span
+    }
+});
+
+  deleteButton.addEventListener("click", () => {
+    listItem.remove();
+  });
+
+  // Append item's to the list
+  listItem.appendChild(checkbox);
+  listItem.appendChild(textSpan);
+  listItem.appendChild(deleteButton);
+
+  list.appendChild(listItem);
+
+  // Clear input field
+  formText.value = "";
 }
 
-// Press Enter To Add Task
+// Press 'enter' to add task
 formText.addEventListener("keydown", function (event) {
   if (event.key === "Enter") {
     event.preventDefault();
